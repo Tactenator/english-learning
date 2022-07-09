8e941b560c07c6fdd6dc5f9e176fafcdd8151116
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-07-09 16:13:04 -0500

    Deleted random JSON file and changed button colors

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <div className="welcome-text">
-                <h2>Bienvenidos a Ingles!</h2>
+                <h2>Bienvenidos a Chillona!</h2>
                 <p>Aqui, vas a aprender las temas basicas de Ingles</p>
                 <img src={home} className="home-image" height="60%" width="60%"alt="Friends celebrating"></img>
             </div>
